e427a80f2b1553f9887330042fc21d112a8756ec
xxxJOKERxxx <0888200@gmail.com>
2026-02-04 00:45:40 +0300

    Структурировал курс по папкам!!

--- src/main/java/com/productstar/step1/block1/lesson1/Main.java
+++ src/main/java/com/productstar/step1/block1/lesson1/Main.java
@@ -2,7 +2,7 @@ package com.productstar.step1.block1.lesson1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("");
 
     }
 }
