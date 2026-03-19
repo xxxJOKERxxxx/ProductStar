6b2faab38e9102b7069043ff0a9e17e2095951d8
xxxJOKERxxx <0888200@gmail.com>
2026-03-19 21:48:58 +0300

    weatherCondition

---
+++ src/main/java/com/productstar/step1/block1/lesson3/WeatherReport.java
@@ -0,0 +1,10 @@
+package com.productstar.step1.block1.lesson3;
+
+public class WeatherReport {
+    public static void main(String[] args) {
+
+        int temperature = 25;
+        String weather = "солнечно,";
+        System.out.println(" сегодня на улиуце " + weather + " температура "+ temperature + " градусов.");
+}
+}
