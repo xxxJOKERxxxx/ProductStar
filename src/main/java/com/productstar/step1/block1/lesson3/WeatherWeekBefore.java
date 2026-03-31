871631e249a932c0cec45cb26ddaaf4760ba344f
xxxJOKERxxx <0888200@gmail.com>
2026-03-31 16:29:02 +0300

    For

---
+++ src/main/java/com/productstar/step1/block1/lesson3/WeatherWeekBefore.java
@@ -0,0 +1,10 @@
+package com.productstar.step1.block1.lesson3;
+
+public class WeatherWeekBefore {
+    public static void main(String[] args) {
+        int[] weatherWeekBefore = new int[]{-20,-15,-17,-10,-5,0};
+        for (int i = 0; i < weatherWeekBefore.length; i++) {
+
+        }
+    }
+}
