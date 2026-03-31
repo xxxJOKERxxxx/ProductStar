97c1fccd44c70ba3904ec2121c88eac17177f23b
xxxJOKERxxx <0888200@gmail.com>
2026-03-31 17:04:45 +0300

    for

---
+++ src/main/java/com/productstar/step1/block1/lesson3/Main.java
@@ -0,0 +1,21 @@
+package com.productstar.step1.block1.lesson3;
+
+public class Main {
+    public static void main(String[] args) {
+        int n = 78;
+        boolean wasFound = false;
+        int[]array = new int[]{1,20,30,-5,-8,2,-7,123,-90};
+        for(int i=0;i<array.length;i++){
+            int numberFromArray = array[i];
+            if(numberFromArray==n){
+                System.out.println("найдено!");
+                wasFound = true;
+            }
+        }
+        if(!wasFound){
+            System.out.println("Не найдено!");
+        }
+
+
+    }
+}
