716ae26ad6ba64de1b6e73d46852eac229f5fe2d
xxxJOKERxxx <0888200@gmail.com>
2026-03-31 16:41:30 +0300

    while

--- src/main/java/com/productstar/step1/block1/lesson3/WeatherWeekBefore.java
+++ src/main/java/com/productstar/step1/block1/lesson3/WeatherWeekBefore.java
@@ -3,8 +3,12 @@ package com.productstar.step1.block1.lesson3;
 public class WeatherWeekBefore {
     public static void main(String[] args) {
         int[] weatherWeekBefore = new int[]{-20,-15,-17,-10,-5,0};
-        for (int i = 0; i < weatherWeekBefore.length; i++) {
+      int i =0;
+      while (i < weatherWeekBefore.length) {
+          int temperature = weatherWeekBefore[i];
+          System.out.println("[холодно "+ temperature + " одеть куртку и шапку!");
+          i++;
+      }
 
-        }
     }
 }
