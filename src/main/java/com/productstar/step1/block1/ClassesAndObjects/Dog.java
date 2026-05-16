1013b44a68f9a776af5f2664924e89b120fee8f6
PC <0888200@gmail.com>
2026-05-16 23:08:07 +0300

    Classes  and Objects

---
+++ src/main/java/com/productstar/step1/block1/ClassesAndObjects/Dog.java
@@ -0,0 +1,14 @@
+package com.productstar.step1.block1.ClassesAndObjects;
+
+import java.sql.SQLOutput;
+
+public class Dog {
+    int age;
+    String color;
+    boolean gender;
+
+    public void bark() {
+        System.out.println("bark!");
+    }
+
+}
---
+++ src/main/java/com/productstar/step1/block1/ClassesAndObjects/Main.java
@@ -0,0 +1,13 @@
+package com.productstar.step1.block1.ClassesAndObjects;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog Sharik = new Dog();
+        Sharik.age = 5;
+        System.out.println("Sharik is " + Sharik.age + " years");
+        Sharik.bark();
+
+
+
+    }
+}
