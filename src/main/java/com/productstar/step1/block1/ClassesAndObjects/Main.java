9368949dc0a2d07fbba5d8fc5e67e82df686f3e1
PC <0888200@gmail.com>
2026-05-17 00:26:10 +0300

    Classes  and Objects pet(Animal animal)

--- src/main/java/com/productstar/step1/block1/ClassesAndObjects/Main.java
+++ src/main/java/com/productstar/step1/block1/ClassesAndObjects/Main.java
@@ -3,21 +3,15 @@ package com.productstar.step1.block1.ClassesAndObjects;
 public class Main {
     public static void main(String[] args) {
         Dog Sharik = new Dog();
-        Sharik.age = 5;
-        System.out.println("Sharik is " + Sharik.age + " years");
-        Sharik.makeSound();
-        Sharik.jump();
-
         Cat Murka = new Cat();
-        Murka.age = 3;
-        System.out.println("Murka is " + Murka.age + " years");
-        Murka.makeSound();
-        Murka.jump();
-
         Hamster hamster = new Hamster();
-        hamster.makeSound();
 
+        pet(Sharik);
+        pet(Murka);
+        pet(hamster);
+    }
 
-
+    public static void pet(Animal animal) {
+        animal.makeSound();
     }
 }
