c2746f4b88e97844d6776624044811ca999ca555
PC <0888200@gmail.com>
2026-06-02 22:16:57 +0300

    синтаксис определения полей и методов класса private setters and getters  добавим модификаторы

--- src/main/java/com/productstar/step1/block1/WorkShop/Main.java
+++ src/main/java/com/productstar/step1/block1/WorkShop/Main.java
@@ -2,8 +2,8 @@ package com.productstar.step1.block1.WorkShop;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(1, 2);
-        rectangle.a = 1;
+        Rectangle rectangle = new Rectangle(-100, 2);
+        rectangle.a = -100;
         rectangle.b = 2;
         System.out.println(rectangle);
     }
--- src/main/java/com/productstar/step1/block1/WorkShop/Rectangle.java
+++ src/main/java/com/productstar/step1/block1/WorkShop/Rectangle.java
@@ -1,13 +1,35 @@
 package com.productstar.step1.block1.WorkShop;
 
 public class Rectangle {
-    public double a;
+    private double a;
 
-    public double b;
+    private double b;
 
     public Rectangle(double a, double b) {
         this.a = a;
         this.b = b;
+    }
+
+    public void setA(double newA) {
+        if (newA <= 0) {
+            throw new IllegalArgumentException("A canot beles than 0");
+        }
+        this.a = newA;
+    }
+
+    public void setB(double newB) {
+        if (newB <= 0) {
+            throw new IllegalArgumentException("A canot beles than 0");
+        }
+        this.b = newB;
+    }
+
+    public double getA() {
+        return this.a;
+    }
+
+    public double getB() {
+        return this.b;
     }
 
     public String toString() {
