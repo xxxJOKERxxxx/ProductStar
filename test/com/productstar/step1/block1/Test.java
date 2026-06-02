80a89186d7956ad877e76b04a1f1ef645ed40fb1
PC <0888200@gmail.com>
2026-06-02 22:45:35 +0300

    синтаксис определения полей и методов класса private setters and getters  добавим модификаторы  protected  package private  test

---
+++ test/com/productstar/step1/block1/Test.java
@@ -0,0 +1,12 @@
+package com.productstar.step1.block1;
+
+import com.productstar.step1.block1.WorkShop.Rectangle;
+
+public class Test {
+
+    public void test(){
+        Rectangle rectangle = new Rectangle(1,2);
+        rectangle.setA(22);
+    }
+
+}
