9d24f258dfbf5d8ec6ff3e390686fabfed7b2e9d
PC <0888200@gmail.com>
2026-06-05 19:42:49 +0300

    Exception try catch ArithmeticException NumberFormatException

--- src/main/java/com/productstar/step1/block1/Exception/ExceptionsMain.java
+++ src/main/java/com/productstar/step1/block1/Exception/ExceptionsMain.java
@@ -2,16 +2,26 @@ package com.productstar.step1.block1.Exception;
 
 public class ExceptionsMain {
 
+    public static String name;
+
     public static void main(String[] args) {
 
         try {
+            name.length();
+            Integer.parseInt("aaaaaaa");
             divideByZero();
         } catch (ArithmeticException e) {
             System.out.println("You can not divide by zero! Sin!");
+        } catch (NumberFormatException e) {
+            System.out.println("Please specify correct number!");
+        } catch (Exception e) {
+            System.out.println("Unknown exception!");
         }
+
+        System.out.println("All OK!");
     }
 
     public static int divideByZero() {
-        return 666/0;
+        return 666 / 0;
     }
 }
