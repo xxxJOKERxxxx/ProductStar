5c7313da7e6ed8503b9b8c34dd4dbf420d8683e7
PC <0888200@gmail.com>
2026-06-05 20:36:53 +0300

    Exception try catch ArithmeticException NumberFormatException throws FileNotFoundException printStackTrace()

--- src/main/java/com/productstar/step1/block1/Exception/ExceptionsMain.java
+++ src/main/java/com/productstar/step1/block1/Exception/ExceptionsMain.java
@@ -1,27 +1,21 @@
 package com.productstar.step1.block1.Exception;
 
+import java.io.FileNotFoundException;
+
 public class ExceptionsMain {
 
     public static String name;
 
     public static void main(String[] args) {
-
         try {
-            name.length();
-            Integer.parseInt("aaaaaaa");
-            divideByZero();
-        } catch (ArithmeticException e) {
-            System.out.println("You can not divide by zero! Sin!");
-        } catch (NumberFormatException e) {
-            System.out.println("Please specify correct number!");
-        } catch (Exception e) {
-            System.out.println("Unknown exception!");
+            readfile(null);
+        } catch (FileNotFoundException error) {
+            error.printStackTrace();
         }
-
-        System.out.println("All OK!");
     }
-
-    public static int divideByZero() {
-        return 666 / 0;
+    public static void readfile(String path) throws FileNotFoundException {
+        if (path == null) {
+            throw new FileNotFoundException();
+        }
     }
 }
