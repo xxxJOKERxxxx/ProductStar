4c1cd361f1b91ebf158f82384cd814fc8d8b2d79
PC <0888200@gmail.com>
2026-06-12 00:23:05 +0300

    Создай массив из 5 целых чисел (любых). Выведи на экран каждый элемент через пробел.

---
+++ src/main/java/com/productstar/step1/block1/arrays/Array.java
@@ -0,0 +1,11 @@
+package com.productstar.step1.block1.arrays;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+
+        }
+    }
+}
