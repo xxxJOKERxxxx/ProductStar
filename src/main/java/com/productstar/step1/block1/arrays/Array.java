76754ee2314b45fcc7f2550eb058b9de2ded7133
PC <0888200@gmail.com>
2026-06-12 00:37:50 +0300

    Найди сумму всех элементов и выведи её на экран.

--- src/main/java/com/productstar/step1/block1/arrays/Array.java
+++ src/main/java/com/productstar/step1/block1/arrays/Array.java
@@ -2,10 +2,12 @@ package com.productstar.step1.block1.arrays;
 
 public class Array {
     public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5};
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i] + " ");
-
+        int[] nums = {3, 8, 1, 9, 2};
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum = nums[i] + sum;
         }
+        System.out.println("сумма всех элементов равна : " + sum);
+
     }
 }
