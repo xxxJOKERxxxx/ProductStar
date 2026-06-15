e55451087b229be3ff68b1c77c91f89b46835700
PC <0888200@gmail.com>
2026-06-15 23:17:19 +0300

    сумма элементов

---
+++ src/main/java/com/productstar/step1/block1/arrays/SumElements.java
@@ -0,0 +1,12 @@
+package com.productstar.step1.block1.arrays;
+
+public class SumElements {
+    public static void main(String[] args) {
+        int[] nums = {3, 8, 1, 9, 2};
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum  += nums[i];
+        }
+        System.out.println("Сумма всех элементов: " + sum);
+    }
+}
