805d0d06b349f88471d852687917d2d70d9f5cbe
PC <0888200@gmail.com>
2026-07-12 01:21:45 +0300

    Задача №11: «Складская смена» (if-else-if) Ты — оператор склада. Вводишь вес палеты. Система должна сказать, куда её везти: до 10 кг → «Зона A: мелкий товар» 10–30 кг → «Зона B: стандарт» 30–50 кг → «Зона C: тяжёлый» 50–100 кг → «Зона D: крупногабарит» более 100 кг → «ПЕРЕГРУЗ! Не принимаем!» Вес = 0 или отрицательный → «Ошибка ввода»

--- src/main/java/com/productstar/step1/block1/lesson0/Main.java
+++ src/main/java/com/productstar/step1/block1/lesson0/Main.java
@@ -5,19 +5,29 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        int count = 0;
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Введите вес палеты: ");
+        while (true) {
 
-        while (true){
             int weight = scanner.nextInt();
-            if (weight > 100)
-                break;
-            count++;
-        }
-        System.out.println("Палет до перегруза более 100кг: " + count);
 
+            if (weight <= 0) {
+                System.out.println("ОШИБКА ВВОДА!");
+                break;
+            } else if (weight < 10) {
+                System.out.println("Зона А: мелкий товар");
+            } else if (weight <= 30) {
+                System.out.println("Зона В: стандарт");
+            }  else if ( weight <= 50) {
+                System.out.println("Зона С:тяжелый");
+            }else if (weight <= 100) {
+                System.out.println("Зона D:крупногабарит");
+            }else
+                System.out.println("Перегруз! Не принимаем!");
+            break;
+
+        }
 
     }
 }
