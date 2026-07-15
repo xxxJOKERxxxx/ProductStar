be22cda09df7fdb6a14ae2650d4fefb63b3930af
PC <0888200@gmail.com>
2026-07-15 21:45:05 +0300

    Задача №14: «Складской калькулятор 2.0» Теперь добавим ошибку ввода: Вес <= 0 → «ОШИБКА: вес должен быть положительным» Вес > 500 → «ОШИБКА: слишком тяжёлая палета, максимум 500 кг» В остальном — как №13.

--- src/main/java/com/productstar/step1/block1/lesson0/Main.java
+++ src/main/java/com/productstar/step1/block1/lesson0/Main.java
@@ -6,21 +6,37 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Введите вес палеты: ");
+
         int weight = scanner.nextInt();
+
+        double baseCost;
+
+        if (weight <= 0) {
+            System.out.println("Ошибка: вес должен быть положительным!");
+            return;
+        } else if (weight > 500) {
+            System.out.println("Ошибка: слишком тяжёлая палета, максимум 500кг");
+            return;
+        }
+
+
         System.out.println("Ночная ли смена?: ");
         boolean isNight = scanner.nextBoolean();
 
-        double baseCost = 0;
-        if (weight<50) {
-              baseCost = 100;
-        } else if (weight<100) {
-            baseCost = 200;
-        }else  if (weight>100) {
-            baseCost = 500;
-        }
-        double cost = baseCost * (isNight? 0.8 : 1.0);
-        System.out.println(" Стоимость: " + cost);
+
+
+            if (weight < 50) {
+                baseCost = 100;
+            } else if (weight < 100) {
+                baseCost = 200;
+            } else {
+                baseCost = 500;
+            }
+
+        double cost = baseCost * (isNight ? 0.8 : 1.0);
+        System.out.println("Стоимость: " + cost);
 
 
     }
