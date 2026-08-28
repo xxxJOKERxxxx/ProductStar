39e50cd79a685d099a6ae1bc011353c380cf9393
PC <0888200@gmail.com>
2026-08-28 18:38:59 +0300

    ЗАДАЧА №32: «Количество чётных чисел»

--- src/main/java/com/productstar/step1/block1/methodsArray/Task32_CountEven.java
+++ src/main/java/com/productstar/step1/block1/methodsArray/Task32_CountEven.java
@@ -1,30 +1,33 @@
-package com.productstar.step1.block1.methodsArray;
+package com.productstar.step1.block1.methodsArray;   // ← пакет
 
-import java.util.Scanner;
+import java.util.Scanner;                             // ← импорт Scanner
 
-public class Task32_CountEven {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите масив чисел: ");
-        int size = scanner.nextInt();
-        int[] number = new int[size];
+public class Task32_CountEven {                       // ← класс
+
+    public static void main(String[] args) {          // ← main (точка входа)
+        Scanner scanner = new Scanner(System.in);     // ← инструмент для ввода
+
+        System.out.println("Введите масив чисел: ");  // ← запрос размера
+        int size = scanner.nextInt();                 // ← читаем размер
+        int[] number = new int[size];                 // ← создаём массив
+
         System.out.println("Введите " + size + " чисел ");
         for (int i = 0; i < number.length; i++) {
-            System.out.println("число " + (i + 1 + ":"));
-            number[i] = scanner.nextInt();
+            System.out.println("число " + (i + 1 + ":"));  // ← подсказка
+            number[i] = scanner.nextInt();                 // ← заполняем массив
         }
-        int result = countEven(number);
-        System.out.println("колличество чётных чисел: " + result);
 
+        int result = countEven(number);                // ← вызываем метод
+        System.out.println("колличество чётных чисел: " + result); // ← вывод
     }
-    public static int countEven (int[] number) {
-        int count =0;
-        for (int i = 0; i < number.length; i++) {
 
-            if (number[i] % 2 == 0) {
-                count++;
+    public static int countEven(int[] number) {        // ← метод
+        int count = 0;                                 // ← счётчик
+        for (int i = 0; i < number.length; i++) {
+            if (number[i] % 2 == 0) {                  // ← проверка на чётность
+                count++;                               // ← увеличиваем
             }
         }
-        return count;
+        return count;                                  // ← возвращаем результат
     }
 }
